0f7c8f72d92d35e5d3a5a16452fcc0b0c0fb3733
YRJ <931622815@qq.com>
2019-11-04 18:00:24 +0800

    update

--- src/main/java/cn/aethli/thoth/service/LotteryServiceImpl.java
+++ src/main/java/cn/aethli/thoth/service/LotteryServiceImpl.java
@@ -3,12 +3,14 @@ package cn.aethli.thoth.service;
 import cn.aethli.thoth.common.enums.LotteryType;
 import java.util.List;
 import java.util.Map;
+import org.springframework.stereotype.Service;
 
 /**
  * @author Termite
  * @device Hades
  * @date 2019-10-29 15:53
  */
+@Service
 public class LotteryServiceImpl implements LotteryService {
 
   @Override
